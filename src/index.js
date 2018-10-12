6665f107731c34badab8d6ec84d24ce894e52760
李金标 <1280597658@qq.com>
2018-10-12 14:19:00 +0800

    修复标题

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-const DocumentTitle = (title) => {
-    const nextTitle = title || '';
-    if (nextTitle !== document.title) {
-        document.title = nextTitle;
-    }
-    return (target) => target;
+import React, { Component } from 'react';
+function getDisplayName(WrappedComponent) {
+    return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
-
-export default DocumentTitle;
+export default (title) => {
+    return (WrappedComponent) => {
+        return class DocumentTitle extends Component {
+            static displayName = `DocumentTitle(${getDisplayName(WrappedComponent)})`;
+            render() {
+                const nextTitle = title || '';
+                if (nextTitle !== document.title) {
+                    document.title = nextTitle;
+                }
+                return <WrappedComponent {...this.props}/>;
+            }
+        }
+    };
+}
